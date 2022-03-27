fe9209fa4677b74b0d219c7348cab796cd4a6493
narcpat <narcissearts@gmail.com>
2022-03-27 19:05:20 -0400

    built HTML & CSS for time blocks

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,4 @@
+// used the jQuery CheatSheet found at https://htmlcheatsheet.com/jquery/
 $(document).ready(function () {
   // show today's date at the top of the calendar
   $("#currentDay").text(moment().format("LLLL")); //formatted based on https://devhints.io/moment
