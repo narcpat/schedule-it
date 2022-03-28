f95ead95757dc560d6b24d4a18b3643d1bafd4d3
narcpat <narcissearts@gmail.com>
2022-03-28 00:08:58 -0400

    simplified save method and updated pseudocode

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,34 +4,18 @@ var tasks = {};
 $(document).ready(function () {
   // show today's date at the top of the calendar
   $("#currentDay").text(moment().format("LLLL")); //formatted based on https://devhints.io/moment
+
+  // when save is clicked
+  $(".saveBtn").click(function () {
+    var taskText = $(this).siblings("textarea").val();
+    var currentHr = $(this).parent().attr("id");
+    console.log(currentHr, taskText);
+
+    // Save to local storage
+    localStorage.setItem(taskText, currentHr);
+
+    // TODO Loop through the different saved divs and getItem
+
+    // TODO set conditions for past, current and future states and trigger corresponding CSS
+  });
 });
-
-// !todo - need to create task function which will be linked to the time entered field use ID.
-var createTask = function (taskText) {
-  var taskP = $("<p>").text(taskText);
-
-  // append p element to textarea
-};
-
-// to save tasks to local storage
-var saveTasks = function () {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-// to retrieve tasks saved in local storage
-var loadTasks = function () {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
-};
-
-// when save is clicked
-$(".saveBtn").click(function () {
-  var taskText = $(this).siblings("textarea").val();
-  var currentHr = $(this).parent().attr("id");
-  console.log(currentHr, taskText);
-  saveTasks();
-});
-
-// load tasks that were previously saved
-loadTasks();
-
-// todo  -- need to use the CSS classes to color code the timeblocks that are past and future
